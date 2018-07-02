f2e55fb1dc1a03f3027cbe6f6a8bd4880cd4d815
Dm1212 <primakov.dm@yandex.ru>
2018-07-02 14:18:19 +0400

    Set results as hash for db

--- app.rb
+++ app.rb
@@ -4,7 +4,9 @@ require 'sinatra/reloader'
 require 'sqlite3'
 
 def get_db 
-	return SQLite3::Database.new 'barbershop.db'
+	db = SQLite3::Database.new 'barbershop.db'
+	db.results_as_hash = true
+	return db
 end
 
 configure do
@@ -67,4 +69,8 @@ post '/visit' do
 
 	erb "OK, username is #{@username}, #{@phone}, #{@datetime}, #{@barber}, #{@color}"
 
+end
+
+get '/showusers' do
+	erb "Hello"
 end
